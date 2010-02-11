cd4a8e14fba2f2ecf412cfe5233f03bab5bb31be
David Weldon <david.weldon@ask.com>
2010-02-11 13:10:21 -0800

    removed namespace as a required parameter.

--- src/ecache.erl
+++ src/ecache.erl
@@ -2,13 +2,13 @@
 -behaviour(gen_server).
 -export([start_link/0,
          stop/0,
-         load/2,
-         store/3,
-         delete/2,
+         load/1,
+         store/2,
+         delete/1,
          count/0,
          flush/0,
-         increment/2,
-         decrement/2]).
+         increment/1,
+         decrement/1]).
 -export([init/1,
          handle_call/3,
          handle_cast/2,
@@ -26,17 +26,17 @@ stop() ->
 init([]) ->
     {ok, new_table()}.
 
-% @spec load(Namespace::any(), Key::any()) -> {ok, any()} | {error, not_found}
-load(Namespace, Key) ->
-    gen_server:call(?MODULE, {load, Namespace, Key}).
+% @spec load(Key::any()) -> {ok, any()} | {error, not_found}
+load(Key) ->
+    gen_server:call(?MODULE, {load, Key}).
 
-% @spec store(Namespace::any(), Key::any(), Value::any()) -> ok
-store(Namespace, Key, Value) ->
-    gen_server:cast(?MODULE, {store, Namespace, Key, Value}).
+% @spec store(Key::any(), Value::any()) -> ok
+store(Key, Value) ->
+    gen_server:cast(?MODULE, {store, Key, Value}).
 
-% @spec delete(Namespace::any(), Key::any()) -> ok
-delete(Namespace, Key) ->
-    gen_server:cast(?MODULE, {delete, Namespace, Key}).
+% @spec delete(Key::any()) -> ok
+delete(Key) ->
+    gen_server:cast(?MODULE, {delete, Key}).
 
 % @spec count() -> integer()
 count() ->
@@ -46,42 +46,42 @@ count() ->
 flush() ->
     gen_server:cast(?MODULE, flush).
 
-% @spec increment(Namespace::any(), Key::any()) -> {ok, integer()} |
-%                                                  {error, not_found} |
-%                                                  {error, not_an_integer}
-increment(Namespace, Key) ->
+% @spec increment(Key::any()) -> {ok, integer()} |
+%                                {error, not_found} |
+%                                {error, not_an_integer}
+increment(Key) ->
     Fun = fun(X) -> X + 1 end,
-    gen_server:call(?MODULE, {increment_or_decrement, Namespace, Key, Fun}).
+    gen_server:call(?MODULE, {increment_or_decrement, Key, Fun}).
 
-% @spec decrement(Namespace::any(), Key::any()) -> {ok, integer()} |
-%                                                  {error, not_found} |
-%                                                  {error, not_an_integer}
-decrement(Namespace, Key) ->
+% @spec decrement(Key::any()) -> {ok, integer()} |
+%                                {error, not_found} |
+%                                {error, not_an_integer}
+decrement(Key) ->
     Fun = fun(X) -> X - 1 end,
-    gen_server:call(?MODULE, {increment_or_decrement, Namespace, Key, Fun}).
+    gen_server:call(?MODULE, {increment_or_decrement, Key, Fun}).
 
 %-------------------------------------------------------------------------------
-handle_call({load, Namespace, Key}, _From, Table) ->
-    case ets:lookup(Table, {Namespace, Key}) of
+handle_call({load, Key}, _From, Table) ->
+    case ets:lookup(Table, Key) of
         [] ->
             {reply, {error, not_found}, Table};
-        [Data|_] ->
-            {{Namespace, Key}, Value} = Data,
+        [Data|[]] ->
+            {Key, Value} = Data,
             {reply, {ok, Value}, Table}
     end;
 handle_call(count, _From, Table) ->
     Count = proplists:get_value(size, ets:info(Table)),
     {reply, Count, Table};
-handle_call({increment_or_decrement, Namespace, Key, Fun}, _From, Table) ->
-    case ets:lookup(Table, {Namespace, Key}) of
+handle_call({increment_or_decrement, Key, Fun}, _From, Table) ->
+    case ets:lookup(Table, Key) of
         [] ->
             {reply, {error, not_found}, Table};
-        [Data|_] ->
-            {{Namespace, Key}, Value} = Data,
+        [Data|[]] ->
+            {Key, Value} = Data,
             case is_integer(Value) of
                 true ->
-                    NewValue = apply(Fun, [Value]),
-                    true = ets:insert(Table, {{Namespace, Key}, NewValue}),
+                    NewValue = Fun(Value),
+                    true = ets:insert(Table, {Key, NewValue}),
                     {reply, {ok, NewValue}, Table};
                 false ->
                     {reply, {error, not_an_integer}, Table}
@@ -90,11 +90,11 @@ handle_call({increment_or_decrement, Namespace, Key, Fun}, _From, Table) ->
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
-handle_cast({store, Namespace, Key, Value}, Table) ->
-    true = ets:insert(Table, {{Namespace, Key}, Value}),
+handle_cast({store, Key, Value}, Table) ->
+    true = ets:insert(Table, {Key, Value}),
     {noreply, Table};
-handle_cast({delete, Namespace, Key}, Table) ->
-    true = ets:delete(Table, {Namespace, Key}),
+handle_cast({delete, Key}, Table) ->
+    true = ets:delete(Table, Key),
     {noreply, Table};
 handle_cast(flush, Table) ->
     true = ets:delete(Table),
@@ -120,66 +120,66 @@ new_table() ->
 
 load_store_test() ->
     ecache:start_link(),
-    ecache:store(autos, <<"ford">>, 10),
-    ecache:store(autos, <<"toyota">>, 9),
-    ecache:store(<<"cpu">>, "intel", "Santa Clara"),
-    ecache:store(<<"cpu">>, "amd", "Sunnyvale"),
-    ?assertEqual({ok, 10}, ecache:load(autos, <<"ford">>)),
-    ?assertEqual({ok, 9}, ecache:load(autos, <<"toyota">>)),
-    ?assertEqual({ok, "Santa Clara"}, ecache:load(<<"cpu">>, "intel")),
-    ?assertEqual({ok, "Sunnyvale"}, ecache:load(<<"cpu">>, "amd")),
-    ?assertEqual({error, not_found}, ecache:load("cpu", "amd")),
-    ecache:store(autos, <<"toyota">>, 1),
-    ?assertEqual({ok, 1}, ecache:load(autos, <<"toyota">>)),
+    ecache:store({autos, <<"ford">>}, 10),
+    ecache:store({autos, <<"toyota">>}, 9),
+    ecache:store("intel", "Santa Clara"),
+    ecache:store("amd", "Sunnyvale"),
+    ?assertEqual({ok, 10}, ecache:load({autos, <<"ford">>})),
+    ?assertEqual({ok, 9}, ecache:load({autos, <<"toyota">>})),
+    ?assertEqual({ok, "Santa Clara"}, ecache:load("intel")),
+    ?assertEqual({ok, "Sunnyvale"}, ecache:load("amd")),
+    ?assertEqual({error, not_found}, ecache:load(<<"amd">>)),
+    ecache:store({autos, <<"toyota">>}, 1),
+    ?assertEqual({ok, 1}, ecache:load({autos, <<"toyota">>})),
     ecache:stop().
 
 delete_test() ->
     ecache:start_link(),
-    ecache:store(fruit, apple, 1),
-    ecache:store(fruit, peach, 1),
-    ecache:delete(fruit, apple),
-    ?assertEqual({ok, 1}, ecache:load(fruit, peach)),
-    ?assertEqual({error, not_found}, ecache:load(fruit, apple)),
+    ecache:store(apple, 1),
+    ecache:store(peach, 1),
+    ecache:delete(apple),
+    ?assertEqual({ok, 1}, ecache:load(peach)),
+    ?assertEqual({error, not_found}, ecache:load(apple)),
     ecache:stop().
 
 count_test() ->
     ecache:start_link(),
     ?assertEqual(0, ecache:count()),
-    ecache:store(fruit, apple, 1),
-    ecache:store(fruit, peach, 1),
+    ecache:store(apple, 1),
+    ecache:store(peach, 1),
     ?assertEqual(2, ecache:count()),
-    ecache:delete(fruit, apple),
+    ecache:delete(apple),
     ?assertEqual(1, ecache:count()),
     ecache:stop().
 
 flush_test() ->
     ecache:start_link(),
-    ecache:store(fruit, apple, 1),
-    ecache:store(fruit, peach, 1),
+    ecache:store(apple, 1),
+    ecache:store(peach, 1),
     ecache:flush(),
     ?assertEqual(0, ecache:count()),
-    ?assertEqual({error, not_found}, ecache:load(fruit, apple)),
-    ?assertEqual({error, not_found}, ecache:load(fruit, peach)),
+    ?assertEqual({error, not_found}, ecache:load(apple)),
+    ?assertEqual({error, not_found}, ecache:load(peach)),
     ecache:stop().
 
 increment_test() ->
     ecache:start_link(),
-    ecache:store(fruit, apple, 10),
-    ?assertEqual({ok, 11}, ecache:increment(fruit, apple)),
-    ?assertEqual({ok, 12}, ecache:increment(fruit, apple)),
-    ?assertEqual({ok, 12}, ecache:load(fruit, apple)),
-    ecache:store(fruit, apple, "10"),
-    ?assertEqual({error, not_an_integer}, ecache:increment(fruit, apple)),
-    ?assertEqual({error, not_found}, ecache:increment(fruit, peach)),
+    ecache:store(apple, 10),
+    ?assertEqual({ok, 11}, ecache:increment(apple)),
+    ?assertEqual({ok, 12}, ecache:increment(apple)),
+    ?assertEqual({ok, 12}, ecache:load(apple)),
+    ecache:store(apple, "10"),
+    ?assertEqual({error, not_an_integer}, ecache:increment(apple)),
+    ?assertEqual({error, not_found}, ecache:increment(peach)),
     ecache:stop().
 
 decrement_test() ->
     ecache:start_link(),
-    ecache:store(fruit, apple, 10),
-    ?assertEqual({ok, 9}, ecache:decrement(fruit, apple)),
-    ?assertEqual({ok, 8}, ecache:decrement(fruit, apple)),
-    ?assertEqual({ok, 8}, ecache:load(fruit, apple)),
-    ecache:store(fruit, apple, "10"),
-    ?assertEqual({error, not_an_integer}, ecache:decrement(fruit, apple)),
-    ?assertEqual({error, not_found}, ecache:decrement(fruit, peach)),
+    ecache:store(apple, 10),
+    ?assertEqual({ok, 9}, ecache:decrement(apple)),
+    ?assertEqual({ok, 8}, ecache:decrement(apple)),
+    ?assertEqual({ok, 8}, ecache:load(apple)),
+    ecache:store(apple, "10"),
+    ?assertEqual({error, not_an_integer}, ecache:decrement(apple)),
+    ?assertEqual({error, not_found}, ecache:decrement(peach)),
     ecache:stop().
